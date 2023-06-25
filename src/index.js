d3aca5bf69c5df714e4fdbdfb73f5bbad8acc4d7
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-25 15:10:41 +0300

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const taskInput = document.getElementById("new-task-description");
+      const taskText = taskInput.value;
+      taskInput.value = "";
+  
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+    });
+  });
+  
 });
